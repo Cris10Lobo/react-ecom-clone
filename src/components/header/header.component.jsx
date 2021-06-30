09dfd810104c49ed0b74c70d0c7a94d1164be8c9
criston <cris10lobo@gmail.com>
2021-06-30 15:09:27 +0530

    Checkpoint: Added cart-icon and cart-dropdown components using cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -28,15 +30,18 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
 //this func allows us to connect to the state
 
-const mapStateToProp = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProp = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProp)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
-
+import cartReducer from './cart/cart.reducer';
 //this is the root reducer object.
 //this is where all the reducer are combined together to form the mega reducer like power rangers megazord.
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
