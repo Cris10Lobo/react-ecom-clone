e08515f0ca90e5deda19d18bace2ce280532fafb
criston <cris10lobo@gmail.com>
2021-07-01 10:42:24 +0530

    Checkpoint: Added add to cart using cartReducer and utils with quantity

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +14,20 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>${price}</span>
       </div>
+      <CustomButton
+        onClick={() => {
+          addItem(item);
+        }}
+        inverted
+      >
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchtoProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchtoProps)(CollectionItem);
